941253369b71b0527373c6ee98722865799ba5bb
rayen <rayenghaziayachi@gmail.com>
2023-10-08 17:39:41 +0100

    first commit

---
+++ fifa/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src='https://a.espncdn.com/photo/2021/0914/r908859_612x918_2-3.png'>
+
+      </img>
+      <img src='https://a.espncdn.com/photo/2021/0914/r908858_612x918_2-3.png'>
+        
+      </img>
+      <img src='https://a.espncdn.com/photo/2021/0914/r908861_612x918_2-3.png'>
+        
+      </img>
+    
+    </div>
+  );
+}
+
+export default App;
